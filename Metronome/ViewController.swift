fcf6820dcda63acb3aca9cfad696ad625d1755a1
Roman Remenkov <remenkov.r@industry-soft.ru>
2024-02-05 21:02:15 +0300

    M-1 Create Project

---
+++ Metronome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Metronome
+//
+//  Created by Roman Remenkov on 05.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
